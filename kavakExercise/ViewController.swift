1fe78b022c0ad6b064bd79be27231fbc85f7a141
Andres Bonilla <abonilla@miflink.com>
2019-12-02 23:14:44 -0600

    init commit

---
+++ kavakExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kavakExercise
+//
+//  Created by Andrés Abraham Bonilla Gómez on 12/2/19.
+//  Copyright © 2019 Andrés Abraham Bonilla Gómez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
